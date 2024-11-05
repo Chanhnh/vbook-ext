3f55b7c596daa5eb1bef4688a67a8647211a356c
Nguyễn Hữu Chánh <56595591+Chanhnh@users.noreply.github.com>
2024-11-05 22:42:00 +0700

    69shu
    Đổi icon, sửa ảnh, giới thiệu, tác giả truyện

--- 69shu/src/detail.js
+++ 69shu/src/detail.js
@@ -3,16 +3,21 @@ load('config.js');
 
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
-    url = url.replace("/txt/","/book/","/b/")
+    // Định nghĩa regex để lấy ID sách và 2 chữ số đầu tiên
+    const regexFull = /\/(\d+)\.html/; // Lấy toàn bộ số
+    const regexTwoDigits = /\/(\d{2})\d+\.html/; // Lấy 2 chữ số đầu tiên
+    // Lấy book_id và book_id2 từ URL
+    let book_id = url.match(regexFull)[1];
+    let book_id2 = url.match(regexTwoDigits)[1];
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gbk');
 
         return Response.success({
             name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
-            cover: $.Q(doc, 'div.bookimg2 > img').attr('src'),
-            author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
-            description: $.Q(doc, 'div.navtxt > p').html(),
+            cover: `${BASE_URL}/bimages/${book_id2}/${book_id}/${book_id}s.jpg`,
+            author: $.Q(doc, 'div.booknav2 > p').text().replace("作者：", "").trim(),
+            description: $.Q(doc, 'div.content > p').html(),
             detail: $.QA(doc, 'div.booknav2 p', {m: x => x.text(), j: '<br>'}),
             host: BASE_URL
         })
