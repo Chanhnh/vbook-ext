2d521a145c25335254fc60698db3f1d9f3202ed8
Chanhnh <56595591+Chanhnh@users.noreply.github.com>
2025-08-29 18:31:17 +0700

    Hot fix yeduge
    sửa next trang home

--- yeduge/src/home.js
+++ yeduge/src/home.js
@@ -1,9 +1,9 @@
 function execute() {
     return Response.success([
         { title: "Anti DDOS", input: "", script: "" },
-        { title: "最新小说", input: "/rank/new/", script: "up.js" },
-        { title: "人气排行", input: "/rank/hot/", script: "up.js" },
-        { title: "完本小说", input: "/rank/complete/", script: "up.js" },
-        { title: "免费小说", input: "/rank/free/", script: "up.js" }
+        { title: "最新小说", input: "/rank/new/", script: "gen.js" },
+        { title: "人气排行", input: "/rank/hot/", script: "gen.js" },
+        { title: "完本小说", input: "/rank/complete/", script: "gen.js" },
+        { title: "免费小说", input: "/rank/free/", script: "gen.js" }
     ]);
 }
